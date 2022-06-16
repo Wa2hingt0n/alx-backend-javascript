1d8b3e60ae4365720676cfb067818653090345c5
Washington Obungu <tobungu@gmail.com>
2022-06-16 21:57:15 +0000

    Implemented the class HolbertonClass

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+// Defines a class HolbertonClass
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return this._size;
+    }
+    if (type === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
