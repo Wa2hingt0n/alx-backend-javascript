c4a6af1ff605b761f9a9a65609e463363188397a
Washington Obungu <tobungu@gmail.com>
2022-06-16 17:21:09 +0000

    Deleted trailing period

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -16,6 +16,6 @@ export default class SkyHighBuilding extends Building {
 
   // method declaration
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this._floors} floors.`);
+    return (`Evacuate slowly the ${this._floors} floors`);
   }
 }
