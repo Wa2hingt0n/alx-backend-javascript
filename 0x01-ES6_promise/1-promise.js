4578ee05b8d31fc58b1be5beb09035753fd4701b
Washington Obungu <tobungu@gmail.com>
2022-06-20 14:02:16 +0000

    Created a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise(((resolve, reject) => {
+    if (success === true) {
+      const response = {
+        status: 200,
+        body: 'success',
+      };
+      resolve(response);
+    } else if (success === false) {
+      reject(new Error(': The fake API is not working currently'));
+    }
+  }));
+
+  return promise;
+}
