e43ff21db4c00b68ea766132060ac52b7d172074
Washington Obungu <tobungu@gmail.com>
2022-06-20 12:31:58 +0000

    Returned a Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+// Returns a promise
+export default function getResponseFromAPI() {
+  const promiseObj = new Promise(((resolve, reject) => {
+  })); /* eslint no-unused-vars: 0 */
+
+  return promiseObj;
+}
