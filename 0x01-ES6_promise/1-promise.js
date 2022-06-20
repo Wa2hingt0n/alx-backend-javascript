d2812d27b1696da23ab8a027a764d938dc623416
Washington Obungu <tobungu@gmail.com>
2022-06-20 14:28:38 +0000

    Added an error handler

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,7 +7,7 @@ export default function getFullResponseFromAPI(success) {
       };
       resolve(response);
     } else if (success === false) {
-      reject(('Error: The fake API is not working currently'));
+        reject(Error('The fake API is not working currently'));
     }
   }));
     promise.catch(((err) => console.log(err)))
