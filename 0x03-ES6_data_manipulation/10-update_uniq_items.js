9cf82ef400a0b0fcdaf4a19714eb4ef80da15ffb
Washington Obungu <tobungu@gmail.com>
2022-06-21 20:55:08 +0000

    Added a throw statement for errors

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,9 @@
 // Defines a function that returns an updated map for all items
 // with an initial quantity at 1
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of map.entries()) {
     if (value === 1) {
       map.set(key, 100);
