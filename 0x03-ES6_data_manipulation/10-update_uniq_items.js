fdb9a8bba4a5556ffb1bf26fd93dae6fdf5c3879
Washington Obungu <tobungu@gmail.com>
2022-06-21 20:11:47 +0000

    Added a function that updates a Map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+// Defines a function that returns an updated map for all items
+// with an initial quantity at 1
+export default function updateUniqueItems(map) {
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
+}
