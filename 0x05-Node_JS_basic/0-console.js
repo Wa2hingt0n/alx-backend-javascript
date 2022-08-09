00960c7c94063c1f63f2577f4c51d0219ad43e44
Washington Obungu <tobungu@gmail.com>
2022-08-09 21:43:02 +0000

    Uploaded task 1 and 2

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+// Prints the sring argument to STDOUT
+function displayMessage(message) {
+  process.stdout.write(`${message}\n`);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Creates a program that is executed through the command line
+// It displays a message, that prompts the user for an input,
+// Then displays the user's input
+
+process.stdout.write('Welcome to Holberton School, what is your name? \n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
