cbbcab3d093009db8f74d5f06bb86c49e8e7e09c
Washington Obungu <tobungu@gmail.com>
2022-08-09 21:30:43 +0000

    Added HTTP server

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,117 @@
+// Creates a small http server using the http module
+
+// Reads a csv database file of students and prints certain metrics
+
+const fs = require('fs');
+
+let count = 0;
+let numSWE = 0;
+let numCS = 0;
+const arrayCS = [];
+const arraySWE = [];
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(
+    path, { encoding: 'utf8', ignoreEmpty: true }, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else if (data) {
+        const dataArray = data.toString().split('\n');
+
+        // Remove empty lines from database
+        for (let i = 0; i < dataArray.length; i += 1) {
+          if (dataArray[i] === '') {
+            dataArray.splice(i, 1);
+            i -= 1;
+          }
+        }
+        const numStudents = dataArray.length - 1;
+        for (const item of dataArray) {
+          if (count === 0) {
+          // pass
+          } else {
+            const newValues = item.split(',');
+            if (newValues[3] === 'CS') {
+              numCS += 1;
+              arrayCS.push(newValues[0]);
+            } else if (newValues[3] === 'SWE') {
+              numSWE += 1;
+              arraySWE.push(newValues[0]);
+            }
+          }
+          count += 1;
+        }
+        const stringArrayCS = arrayCS.join(', ');
+        const stringArraySWE = arraySWE.join(', ');
+        console.log(`Number of students: ${numStudents}`);
+        console.log(
+          `Number of students in CS: ${numCS}. List: ${stringArrayCS}`,
+        );
+        console.log(
+          `Number of students in SWE: ${numSWE}. List: ${stringArraySWE}`,
+        );
+        resolve(true);
+      }
+    },
+  );
+});
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 1245;
+const DATABASE = process.argv.length > 2 ? process.argv[2] : '';
+const app = http.createServer();
+
+const ROUTE_HANDLERS = [
+  {
+    route: '/',
+    handler(_, res) {
+      const responseText = 'Hello Holberton School!';
+
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    },
+  },
+  {
+    route: '/students',
+    handler(_, res) {
+      const responseMsg = ['This is the list of our students'];
+
+      countStudents(DATABASE)
+        .then((report) => {
+          responseMsg.push(report);
+          const responseText = responseMsg.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
+        })
+        .catch((err) => {
+          responseMsg.push(err instanceof Error ? err.message : err.toString());
+          const responseText = responseMsg.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
+        });
+    },
+  },
+];
+
+app.on('request', (req, res) => {
+  for (const routeHandler of ROUTE_HANDLERS) {
+    if (routeHandler.route === req.url) {
+      routeHandler.handler(req, res);
+      break;
+    }
+  }
+});
+
+app.listen(port, hostname, () => {
+  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
+});
+
+module.exports = app;
