bb43fa5e8a82ba3b34f50ba0e400bd253bbb0a6b
Washington Obungu <tobungu@gmail.com>
2022-08-09 10:35:43 +0000

    Used readFileSync method

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,43 +1,53 @@
 // Reads a csv database file of students and prints certain metrics
 
 const fs = require('fs');
-const { parse } = require('csv-parse');
+
+let count = 0;
+let numSWE = 0;
+let numCS = 0;
+const arrayCS = [];
+const arraySWE = [];
 
 function countStudents(path) {
-    if (!fs.existsSync(path)) {
-	throw new Error('Cannot load the database');
-    }
-    if (!fs.statSync(path)) {
-	throw new Error('Cannot load the database');
-    }
-    let numStudents = 0;
-    let numSWE = 0;
-    let numCS = 0;
-    const arrayCS = [];
-    const arraySWE = []
-    const parser = parse({columns: true}, function(err, records) {
-	for (let row of records) {
-	    numStudents += 1;
-	    if (row === '\n') {
-		continue;
-	    } else if (row.field === 'CS') {
-		numCS += 1;
-		arrayCS.push(row.firstname);
-	    } else if (row.field === 'SWE') {
-		numSWE += 1;
-		arraySWE.push(row.firstname.split(','));
-	    }
-	}
-	const stringArrayCS = arrayCS.join(', ');
-	const stringArraySWE = arraySWE.join(', ');
-	console.log(`Number of students: ${numStudents}`);
-	console.log(
-	    `Number of students in CS: ${numCS}. List: ${stringArrayCS}`);
-	console.log(
-	    `Number of students in SWE: ${numSWE}. List: ${stringArraySWE}`);
-    });
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(
+    path, { encoding: 'utf8', ignoreEmpty: true },
+  ).toString().split('\n');
 
-    fs.createReadStream(path).pipe(parser);
-};
+  // Remove empty lines from database
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i] === '') {
+      data.splice(i, 1);
+      i -= 1;
+    }
+  }
+  const numStudents = data.length - 1;
+  for (const item of data) {
+    if (count === 0) {
+      // pass
+    } else {
+      const newValues = item.split(',');
+      if (newValues[3] === 'CS') {
+        numCS += 1;
+        arrayCS.push(newValues[0]);
+      } else if (newValues[3] === 'SWE') {
+        numSWE += 1;
+        arraySWE.push(newValues[0]);
+      }
+    }
+    count += 1;
+  }
+  const stringArrayCS = arrayCS.join(', ');
+  const stringArraySWE = arraySWE.join(', ');
+  console.log(`Number of students: ${numStudents}`);
+  console.log(
+    `Number of students in CS: ${numCS}. List: ${stringArrayCS}`,
+  );
+  console.log(
+    `Number of students in SWE: ${numSWE}. List: ${stringArraySWE}`,
+  );
+}
 
 module.exports = countStudents;
