68e163b5a8c562619e6f06ff1579615d9db9565d
Washington Obungu <tobungu@gmail.com>
2022-08-09 14:27:10 +0000

    Added an http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+// Creates a small http server using the http module
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
+
+module.exports = app
