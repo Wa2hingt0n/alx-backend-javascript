67e35b588018e3eaaf3c007a18ac2c6324ae84e0
Washington Obungu <tobungu@gmail.com>
2022-08-11 21:29:42 +0000

    Task 10 upload

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
     res.send('Welcome to the payment system');
 });
@@ -17,12 +19,11 @@ app.get('/available_payments', (_req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  let username = '';
+    let username = '';
 
   if (req.body) {
     username = req.body.userName;
   }
-
   res.send(`Welcome ${username}`);
 });
 
