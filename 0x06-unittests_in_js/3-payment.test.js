f5a5d71a72679bdce04c7f91f121b20ebb1f3f7a
Washington Obungu <tobungu@gmail.com>
2022-08-11 16:04:37 +0000

    Renamed sinon variable

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
-const sinion = require('sinon');
+const sinon = require('sinon');
 const Utils = require('./utils');
 const expect = require('chai').expect;
 const sendPaymentRequestToAPI = require('./3-payment');
 
 describe('Tests sendPaymentRequestToAPI function', () => {
     it('Returns a result using Utils.calculateNumber', () => {
-	const spy = sinion.spy(Utils);
+	const spy = sinon.spy(Utils);
 	sendPaymentRequestToAPI(100, 20);
 	expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
 	expect(spy.calculateNumber.callCount).to.be.equal(1);
