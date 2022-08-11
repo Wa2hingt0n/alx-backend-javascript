1975bec3178ea3e1793b62e8149e144688db7b3d
Washington Obungu <tobungu@gmail.com>
2022-08-11 15:26:16 +0000

    Task 3 upload

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+const sinion = require('sinon');
+const Utils = require('./utils');
+const expect = require('chai').expect;
+const sendPaymentRequestToAPI = require('./3-payment');
+
+describe('Tests sendPaymentRequestToAPI function', () => {
+    it('Returns a result using Utils.calculateNumber', () => {
+	const spy = sinion.spy(Utils);
+	sendPaymentRequestToAPI(100, 20);
+	expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+	expect(spy.calculateNumber.callCount).to.be.equal(1);
+	spy.calculateNumber.restore();
+    });
+});
